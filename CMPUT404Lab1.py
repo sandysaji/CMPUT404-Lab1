9a566ee3453de83ab74aeaf541a4f5df5ee3cd1c
Sandy <Sandy@Sandys-MacBook-Air.local>
2020-01-06 17:53:00 -0700

    Complete

--- CMPUT404Lab1.py
+++ CMPUT404Lab1.py
@@ -1,5 +1,5 @@
 import requests
 print(requests.__version__)
 
-var = requests.get("http://google.com/")
-print(var.status_code)
+var = requests.get("https://raw.githubusercontent.com/sandysaji/CMPUT404-Lab1/master/CMPUT404Lab1.py")
+print(var.content)
